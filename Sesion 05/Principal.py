8757fe0a8252955a34ea14c7c6a7d0aa424fbbe8
E Terrazas <eterrazas.met.pe21@gmail.com>
2022-11-21 00:17:21 -0500

    Actualizando modulos

--- Sesion 05/Principal.py
+++ Sesion 05/Principal.py
@@ -1,9 +1,11 @@
 from diccionarios import cantidadDias
-
-
-
+from diccionarios import determinarMes
 
 def determinarFecha(fecha):
-
-
-    return "Domingo, 30 de octubre de 2022"
+    fecha_string = str(fecha)
+    separador = "/"
+    cadena = fecha_string.split(separador)
+    lista_cadena = list(cadena)
+    return print(lista_cadena)
+    #return "Domingo, 30 de octubre de 2022"
+determinarFecha(30/10/2022)
